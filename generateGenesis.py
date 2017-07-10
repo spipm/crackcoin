c42d2d7887935555beae6101380aa53dda130900
DutchGraa <koffieprog@gmail.com>
2017-07-10 19:00:06 +0200

    Update generateGenesis.py
    Changed genesis generation for simplicity

--- generateGenesis.py
+++ generateGenesis.py
@@ -5,17 +5,16 @@ import crackcoin
 import crackcoin.wallets
 import crackcoin.transactions
 
-coolAddressNames = ['crackcoin','crackcoiN','crackco1n','crackco1N']
+#coolAddressNames = ['crackcoin','crackcoiN','crackco1n','crackco1N']
 
-while 1:
-	privateKey, publicKey = crackcoin.wallets.crackcoin.ecc.make_keypair()
-	compressedPublicKey  = crackcoin.wallets.compressPublicKey(publicKey)
-	newAddress = crackcoin.wallets.publicKeyToAddress(compressedPublicKey)
+#while 1:
+privateKey, publicKey = crackcoin.wallets.crackcoin.ecc.make_keypair()
+compressedPublicKey  = crackcoin.wallets.compressPublicKey(publicKey)
+newAddress = crackcoin.wallets.publicKeyToAddress(compressedPublicKey)
 
-	print newAddress[:9]
-	if newAddress[:9] in coolAddressNames:
-		print privateKey
-		print publicKey
-		print compressedPublicKey
-		print newAddress
-		break
+#print newAddress[:9]
+#if newAddress[:9] in coolAddressNames:
+print privateKey
+#print publicKey
+print compressedPublicKey
+print newAddress
